cbd8deb8cdf240f064b89a0f9b95a9db93079b2f
KuroKami <grisella.pd@gmail.com>
2024-11-29 20:21:11 +0000

    Log In and Sign Up separated at last

--- frontend/epicb-frontend/src/app/app.routes.ts
+++ frontend/epicb-frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { CharacterManagementComponent } from './character-management/character-management.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent }, // Página de inicio para el logueo
   { path: 'admin-dashboard', component: AdminDashboardComponent },
-  { path: 'character-management', component: CharacterManagementComponent }
+  { path: 'character-management', component: CharacterManagementComponent },
+  { path: 'sign-up', component: SignUpComponent}
 ];
